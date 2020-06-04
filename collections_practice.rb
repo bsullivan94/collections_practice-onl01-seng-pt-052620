700af0802229c60b35a452dff48a35761675ed21
brody sullivan <brodysullivan0@gmail.com>
2020-06-04 19:53:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1]) #[1,7,25]
+sort_array_asc([25, 7, 1]) 
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,7 +10,7 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) #[25, 14, 7]
+sort_array_desc([25, 7, 14]) 
 
 def sort_array_char_count(array)
   array.sort do |left, right|
@@ -18,20 +18,20 @@ def sort_array_char_count(array)
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
+sort_array_char_count(["dogs", "cat", "Horses"]) 
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
+swap_elements(["blake", "ashley", "scott"])
 
 def reverse_array(array)
   array.reverse
 end
 
-reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
+reverse_array(["blake", "ashley", "scott"]) 
 
 def kesha_maker(array)
   array.each do |item|
@@ -39,20 +39,16 @@ def kesha_maker(array)
   end
 end
 
-kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
+kesha_maker(["blake", "ashley", "scott"])
 
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
 end
 
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
 
 def sum_array(array)
   sum = 0
@@ -62,7 +58,7 @@ def sum_array(array)
   sum
 end 
  
-sum_array([11,4,7,8,9,100,134]) #273
+sum_array([11,4,7,8,9,100,134]) 
 
 def add_s(array)
   array.collect do |word|
